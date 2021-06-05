66e712cf50e5439871a504377ebc6947a29a3374
Rio6 <rio.liu@r26.me>
2021-06-05 19:46:47 -0400

    tool for uploading code

---
+++ tools/upload.py
@@ -0,0 +1,14 @@
+Import("env")
+import os
+
+address = env.Dump("UPLOAD_PORT").replace('\'', '')
+
+def ssh_upload(target, source, env):
+    print(f"Attempting to upload firmware via SSH to {address}")
+    firmware = str(source[0])
+    os.system('scp '+firmware+' root@'+address+':/root/firmware.hex')
+    os.system('ssh root@'+address+' run-avrdude /root/firmware.hex')
+    exit()
+
+env.AddPreAction("upload", ssh_upload)
+
